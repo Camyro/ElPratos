7eb43bb34136f70b437ebebe43ca335fe56a48fa
Camyro <c1studioscontato@gmail.com>
2023-12-23 01:06:26 +0000

    1.3.1

---
+++ informaçoes/links.js
@@ -0,0 +1,69 @@
+const link1 = document.querySelector(".link1");
+const link2 = document.querySelector(".link2");
+const link3 = document.querySelector(".link3");
+const link4 = document.querySelector(".link4");
+const link5 = document.querySelector(".link5");
+const link6 = document.querySelector(".link6");
+const link7 = document.querySelector(".link7");
+const link8 = document.querySelector(".link8");
+const link9 = document.querySelector(".link9");
+const link10 = document.querySelector(".link10");
+const link11 = document.querySelector(".link11");
+const link12 = document.querySelector(".link12");
+const link13 = document.querySelector(".link13");
+
+hamburger.addEventListener("click", function() {
+  evento();
+});
+
+link1.addEventListener("click", function() {
+  evento();
+});
+
+link2.addEventListener("click", function() {
+  evento();
+});
+
+link3.addEventListener("click", function() {
+  evento();
+});
+
+link4.addEventListener("click", function() {
+  evento();
+});
+
+link5.addEventListener("click", function() {
+  evento();
+});
+
+link6.addEventListener("click", function() {
+  evento();
+});
+
+link7.addEventListener("click", function() {
+  evento();
+});
+
+link8.addEventListener("click", function() {
+  evento();
+});
+
+link9.addEventListener("click", function() {
+  evento();
+});
+
+link10.addEventListener("click", function() {
+  evento();
+});
+
+link11.addEventListener("click", function() {
+  evento();
+});
+
+link12.addEventListener("click", function() {
+  evento();
+});
+
+link13.addEventListener("click", function() {
+  evento();
+});
---
+++ links.js
@@ -0,0 +1,44 @@
+const link1 = document.querySelector(".link1");
+const link2 = document.querySelector(".link2");
+const link3 = document.querySelector(".link3");
+const link4 = document.querySelector(".link4");
+const link5 = document.querySelector(".link5");
+const link6 = document.querySelector(".link6");
+const link7 = document.querySelector(".link7");
+const link8 = document.querySelector(".link8");
+
+hamburger.addEventListener("click", function() {
+  evento();
+});
+
+link1.addEventListener("click", function() {
+  evento();
+});
+
+link2.addEventListener("click", function() {
+  evento();
+});
+
+link3.addEventListener("click", function() {
+  evento();
+});
+
+link4.addEventListener("click", function() {
+  evento();
+});
+
+link5.addEventListener("click", function() {
+  evento();
+});
+
+link6.addEventListener("click", function() {
+  evento();
+});
+
+link7.addEventListener("click", function() {
+  evento();
+});
+
+link8.addEventListener("click", function() {
+  evento();
+});
