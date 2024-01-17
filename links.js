f3f47792de08ccf98f5127e028a4c0b62c19839e
Camyro <c1studioscontato@gmail.com>
2024-01-17 17:00:08 +0000

    1.9.15

--- links.js
+++ links.js
@@ -8,6 +8,13 @@ const link7 = document.querySelector(".link7");
 const link8 = document.querySelector(".link8");
 const link9 = document.querySelector(".link9");
 const link10 = document.querySelector(".link10");
+const link11 = document.querySelector(".link11");
+const link12 = document.querySelector(".link12");
+const link13 = document.querySelector(".link13");
+const link14 = document.querySelector(".link14");
+const link15 = document.querySelector(".link15");
+const link16 = document.querySelector(".link16");
+const link17 = document.querySelector(".link17");
 
 hamburger.addEventListener("click", function() {
   evento();
@@ -50,5 +57,33 @@ link9.addEventListener("click", function() {
 });
 
 link10.addEventListener("click", function() {
+  evento();
+});
+
+link11.addEventListener("click", function() {
+  evento();
+});
+
+link12.addEventListener("click", function() {
+  evento();
+});
+
+link13.addEventListener("click", function() {
+  evento();
+});
+
+link14.addEventListener("click", function() {
+  evento();
+});
+
+link15.addEventListener("click", function() {
+  evento();
+});
+
+link16.addEventListener("click", function() {
+  evento();
+});
+
+link17.addEventListener("click", function() {
   evento();
 });
