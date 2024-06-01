0930ca2ade2ab5376e5d66958bbfa0aceceb9367
Camyro - C1Studios <c1studioscontato@gmail.com>
2024-06-01 17:29:31 +0000

    1.12.0

---
+++ button.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('.button');
+
+for (const button of buttons) {
+  button.addEventListener('click', () => {
+    // Remover a classe "active" de todos os botões
+    buttons.forEach(btn => btn.classList.remove('active'));
+
+    // Adicionar a classe "active" ao botão clicado
+    button.classList.add('active');
+  });
+}
